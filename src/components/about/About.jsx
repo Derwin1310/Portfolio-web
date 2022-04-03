ca4b60be00ca936270e5fde75de1ca78cdaae37b
Derwin Romero <derwin1310@gmail.com>
2022-04-03 17:54:14 -0400

    about responsive deleted and only one component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,29 +1,31 @@
 import {SectionIcons} from './SectionIcons';
 import {TechIcons} from './TechIcons';
-import './styles/About.css';
+import {langContext} from '../helpers/langContext';
+import {useContext} from 'react';
+import './style.css';
 
-export function About () {
+export function About() {
+	const {language} = useContext(langContext);
+
+	const {title, technologies, blueText} = language.about;
+
 	return (
 		<div className='background-color about'>
 			<div id='about' className='section'>
-				<h2 className='subtitle'>About</h2>
+				<h2 className='subtitle'>{title}</h2>
 
 				<SectionIcons />
 
-				<div className='hexagon-grid'>
+				<div className='hexagon-grid responsive-desc '>
 					<div className='hexagon'>
 						<h4 className='hexagon__name'>Derwin Romero</h4>
-						<p className='hexagon__ p'>
-							This is all the technologys that i use to make everything looks
-							like magic in the internet,{' '}
-							<span className='quote__span'>
-								always looking for more to make big things with then.
-							</span>
+						<p className='hexagon__p'>
+							{technologies}
+							<span className='quote__span'>{blueText}</span>
 						</p>
 					</div>
-
-					<TechIcons />
 				</div>
+				<TechIcons />
 			</div>
 		</div>
 	);
