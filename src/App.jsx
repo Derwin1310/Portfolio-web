861fe64fef88a2ebd4233075f73bfc878fe56190
Derwin Romero <derwin1310@gmail.com>
2022-04-07 01:36:42 -0400

    applying the toggle theme and refactoring the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import {Home, Navbar, BurgerBtn, About, Projects, Experience, Article, Footer} from './components';
-import { LangProvider } from './components/helpers/langContext';
+import { LangProvider } from './components/helpers';
 import './styles.css';
 
 export function App () {
 
 	return (
 		<LangProvider>
-			<Home />
-			<Navbar />
-			<BurgerBtn />
-			<About />
-			<Projects />
-			<Experience />
-			<Article />
-			<Footer />
+			<div id='theme' className='theme theme--animation'>
+				<Home />
+				<Navbar />
+				<BurgerBtn />
+				<About />
+				<Projects />
+				<Experience />
+				<Article />
+				<Footer />
+			</div>
 		</LangProvider>
 	);
 };
